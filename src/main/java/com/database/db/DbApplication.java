10d8d93bce26aff0fc2cfe6d4ca61fdae6f0569f
nik.marinenko <nik.marinenko@gmail.com>
2021-03-02 21:10:23 +0300

    first commit

---
+++ src/main/java/com/database/db/DbApplication.java
@@ -0,0 +1,14 @@
+package com.database.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World.");
+
+	}
+
+}
